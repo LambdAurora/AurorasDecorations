21fb04b93968ad3823e558823f3ceff251516b52
LambdAurora <aurora42lambda@gmail.com>
2021-03-04 20:33:42 +0100

    Begone warnings.

--- src/main/java/dev/lambdaurora/aurorasdeco/client/renderer/FakeLeashKnotEntityRenderer.java
+++ src/main/java/dev/lambdaurora/aurorasdeco/client/renderer/FakeLeashKnotEntityRenderer.java
@@ -43,6 +43,7 @@ public class FakeLeashKnotEntityRenderer
         this.shadowRadius = 0.f;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public void render(FakeLeashKnotEntity fakeLeashKnot, float f, float tickDelta, MatrixStack matrices,
                        VertexConsumerProvider vertexConsumers, int light) {
--- src/main/java/dev/lambdaurora/aurorasdeco/mixin/ModResourcePackCreatorMixin.java
+++ src/main/java/dev/lambdaurora/aurorasdeco/mixin/ModResourcePackCreatorMixin.java
@@ -35,7 +35,7 @@ import java.util.function.Consumer;
  * <p>
  * It's just me not having actually worked on the virtual resource pack API in Fabric API so I have to do stupid stuff like this.
  */
-@Mixin(ModResourcePackCreator.class)
+@Mixin(value = ModResourcePackCreator.class, remap = false)
 public class ModResourcePackCreatorMixin {
     @Shadow
     @Final
